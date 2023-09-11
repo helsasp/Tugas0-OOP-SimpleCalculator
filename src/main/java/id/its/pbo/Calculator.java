340d2dc8f00f5bde47bf4aa8b170df0f3c88dc7c
helsasp <helsasp@gmail.com>
2023-09-11 23:07:14 +0700

    feat: add simple caculator

---
+++ src/main/java/id/its/pbo/Calculator.java
@@ -0,0 +1,22 @@
+package id.its.pbo;
+
+public class Calculator {
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int substract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public double divide(int a, int b) {
+		
+		return (double) a / b;
+	}
+}
+
